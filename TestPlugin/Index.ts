02cd60b4c2ea1f3b8c2faac60c9f23f3fc8c7153
Woowz11 <52042904+Woowz11@users.noreply.github.com>
2026-04-12 11:31:09 +0300

    among imposter

---
+++ TestPlugin/Index.ts
@@ -0,0 +1,8 @@
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "name",
+    description: "description",
+    authors: [Devs.author],
+});
